7b07e445aefcb258d196d784a7747f742be26159
theideabased <ogunmusireseyi01@gmail.com>
2024-08-15 18:01:56 +0000

    creating add with float annotation

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+
+def add(a: float, b: float) -> float:
+    """
+    This function will add two variables
+    both in floats and it will return floats
+    variable 'a' is float and variable 'b' is float
+    'a' + 'b' must also be float
+    """
+    return a+b
