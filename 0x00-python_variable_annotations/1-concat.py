77c79906933d1c3bdee5d8b786ac4ca966c203eb
theideabased <ogunmusireseyi01@gmail.com>
2024-08-15 18:42:38 +0000

    concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+def concat(str1: str, str2: str)-> str:
+    """
+    This function is meant to concat two 
+    strings and return a concatenated string
+    """
+    return str1+str2
