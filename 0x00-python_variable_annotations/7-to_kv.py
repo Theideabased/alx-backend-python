42ae42c8b643e737975b293d50b8f6f9f6e7c4ed
theideabased <ogunmusireseyi01@gmail.com>
2024-08-16 20:07:56 +0000

    annotate str and float

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    this will take a string k and an interger
+    of flat v and run both as a tuple the k must be
+    the first element of the tuple and the element 
+    is the square of the int or float
+    """
+    return (k, float(v**2))
