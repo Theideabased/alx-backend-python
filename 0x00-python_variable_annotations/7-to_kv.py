88d8524826483375411fdb510ddd45a4463fcc26
theideabased <ogunmusireseyi01@gmail.com>
2024-08-16 20:13:20 +0000

    annotate to Tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,7 +6,7 @@ def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     this will take a string k and an interger
     of flat v and run both as a tuple the k must be
-    the first element of the tuple and the element 
+    the first element of the tuple and the element
     is the square of the int or float
     """
     return (k, float(v**2))
